b9f4a1337b7f3bbb4d6380e2bbcae9fdd8cabc9d
Pedro Mayorga <ppmayorga80@gmail.com>
2024-06-02 02:56:33 -0600

    add a class

--- aax.py
+++ aax.py
@@ -5,18 +5,18 @@ and a file with all the digits of that number
 additionally provide colors and output size
 
 Usage:
-    aax.py [--no-summary] [--width=W] [--height=H] [--bg=NAME] [--fg=NAME] [--nbg=NAME] [--nfg=NAME] [--image=PATH] [--digits=PATH]
+    aax.py [--summary] [--width=W] [--height=H] [--bg=NAME] [--fg=NAME] [--xbg=NAME] [--xfg=NAME] [--image=PATH] [--digits=PATH]
 
 Options:
     --width=W         output width [default: 25]
     --height=H        output height [default: 16]
     --bg=NAME         background color [default: BLACK]
     --fg=NAME         foreground color [default: WHITE]
-    --nbg=NAME        number's background color [default: BLACK]
-    --nfg=NAME        number's background color [default: RED]
+    --xbg=NAME        number's background color [default: BLACK]
+    --xfg=NAME        number's background color [default: RED]
     --image=PATH      the input image white/black [default: ./pi.png]
     --digits=PATH     the input file for the digits [default: ./pi.txt]
-    --no-summary      don't print the summary
+    --summary         print the summary, i.e. how many digits we need to print
 
 Valid Color Names:
     WHITE
@@ -30,120 +30,139 @@ Valid Color Names:
 """
 
 import os
+from functools import cache
+
 import cv2
 import re
 import logging
-from docopt import docopt
 
+from docopt import docopt
+from tabulate import tabulate
 from colorama import init, Fore, Back
+from colorama.ansi import AnsiBack, AnsiFore
+from typing import Union
 
 init(autoreset=True)
-
-
-def str2bg(name: str) -> str:
-    bg_colors = {
-        "BLACK": Back.BLACK,
-        "WHITE": Back.WHITE,
-        "BLUE": Back.BLUE,
-        "CYAN": Back.CYAN,
-        "GREEN": Back.GREEN,
-        "MAGENTA": Back.MAGENTA,
-        "RED": Back.RED,
-        "YELLOW": Back.YELLOW
-    }
-    return bg_colors[name.strip().upper()]
-
-
-def str2fg(name: str) -> str:
-    fg_colors = {
-        "BLACK": Fore.BLACK,
-        "WHITE": Fore.WHITE,
-        "BLUE": Fore.BLUE,
-        "CYAN": Fore.CYAN,
-        "GREEN": Fore.GREEN,
-        "MAGENTA": Fore.MAGENTA,
-        "RED": Fore.RED,
-        "YELLOW": Fore.YELLOW
-    }
-    return fg_colors[name.strip().upper()]
 
 
 def fix_path_fn(path: str) -> str:
     return path.replace("${HOME}", os.environ["HOME"]).replace("~", os.environ["HOME"])
 
 
-def aax(image_path: str, digits_path: str, width: int, height: int, bg: str, fg: str, number_bg: str,
-        number_fg: str) -> tuple[str, dict, dict]:
-    image_path = fix_path_fn(image_path)
-    digits_path = fix_path_fn(digits_path)
+class AAx:
+    def __init__(self, image_path: str, digits_path: str, bg: str, fg: str, x_bg: str, x_fg: str):
+        self.image_path = image_path
+        self.digits_path = digits_path
+        self.bg = self.str_to_color(color=bg, src=Back)
+        self.fg = self.str_to_color(color=fg, src=Fore)
+        self.x_bg = self.str_to_color(color=x_bg, src=Back)
+        self.x_fg = self.str_to_color(color=x_fg, src=Fore)
 
-    digits = re.sub(r"[.\s]+", "", open(digits_path).read())
-    if len(digits) < width * height:
-        msg = f"""
-        the number of digits in '{digits_path}' ({len(digits)}) is less than 
-        the output size {width}x{height}={width * height}\nSolution: add more
-        digits to the file or reduce output size
-        """
-        logging.error(msg)
-        return "", {}, {}
+        self.output = ""
+        self.freq_x_digits = {}
+        self.freq_bg_digits = {}
 
-    image_x = cv2.imread(image_path, cv2.COLOR_BGR2GRAY)
-    image_a = cv2.cvtColor(image_x, cv2.COLOR_BGR2GRAY)
+    @classmethod
+    @cache
+    def str_to_color(cls, color: str, src: Union[AnsiFore, AnsiBack]) -> str:
+        src = {
+            "BLACK": src.BLACK,
+            "WHITE": src.WHITE,
+            "BLUE": src.BLUE,
+            "CYAN": src.CYAN,
+            "GREEN": src.GREEN,
+            "MAGENTA": src.MAGENTA,
+            "RED": src.RED,
+            "YELLOW": src.YELLOW
+        }
+        return src[color.strip().upper()]
 
-    image_b = cv2.resize(image_a, (width, height), interpolation=cv2.INTER_AREA)
-    image_c = cv2.threshold(image_b, 192, 255, cv2.THRESH_BINARY)[1]
+    def run(self, width: int, height: int) -> str:
+        digits = re.sub(r"[.\s]+", "", open(self.digits_path).read())
+        if len(digits) < width * height:
+            msg = f"""
+            the number of digits in '{self.digits_path}' ({len(digits)}) is less than 
+            the output size {width}x{height}={width * height}\nSolution: add more
+            digits to the file or reduce output size
+            """
+            logging.error(msg)
+            self.output = ""
+            self.x_bg = {}
+            self.x_fg = {}
+            return ""
 
-    fg_digits = {str(x): 0 for x in range(10)}
-    bg_digits = {str(x): 0 for x in range(10)}
+        image_x = cv2.imread(self.image_path, cv2.COLOR_BGR2GRAY)
+        image_a = cv2.cvtColor(image_x, cv2.COLOR_BGR2GRAY)
 
-    output = ""
-    k, nl = 0, ""
-    for i in range(height):
-        output += nl
-        nl = "\n"
-        for j in range(width):
-            digit = digits[k]
-            k += 1
+        image_b = cv2.resize(image_a, (width, height), interpolation=cv2.INTER_AREA)
+        image_c = cv2.threshold(image_b, 192, 255, cv2.THRESH_BINARY)[1]
 
-            if image_c[i, j] != 0:
-                ascii_char = str2fg(fg) + str2bg(bg)
-                bg_digits[digit] += 1
-            else:
-                ascii_char = str2fg(number_fg) + str2bg(number_bg)
-                fg_digits[digit] += 1
+        freq_fg_digits = {str(x): 0 for x in range(10)}
+        freq_bg_digits = {str(x): 0 for x in range(10)}
 
-            ascii_char += digit
-            output += ascii_char
+        output = ""
+        k, nl = 0, ""
+        for i in range(height):
+            output += nl
+            nl = "\n"
+            for j in range(width):
+                digit = digits[k]
+                k += 1
 
-    return output, fg_digits, bg_digits
+                if image_c[i, j] != 0:
+                    fg_color = self.fg
+                    bg_color = self.bg
+                    freq_bg_digits[digit] += 1
+                else:
+                    fg_color = self.x_fg
+                    bg_color = self.x_bg
+                    freq_fg_digits[digit] += 1
+
+                output += fg_color + bg_color + digit
+
+        self.output = output
+        self.freq_x_digits = freq_fg_digits
+        self.freq_bg_digits = freq_bg_digits
+
+        return output
+
+    def summary_table(self):
+        lines = []
+
+        x_tuple = ("X Digit", self.freq_x_digits, self.x_fg, self.x_bg)
+        o_tuple = ("Digit", self.freq_bg_digits, self.fg, self.bg)
+
+        for row_title, freq, fg, bg in (x_tuple, o_tuple):
+            line = []
+            for x in range(10):
+                sx = str(x)
+                fx = str(freq[sx])
+
+                text = bg + fg + f"[{sx}]={fx}"
+
+                line.append(text)
+
+            lines.append(line)
+
+        table_text = tabulate(lines, tablefmt="simple")
+        return table_text
 
 
 if __name__ == "__main__":
     args = docopt(__doc__)
 
-    out, fgd, bgd = aax(image_path=args["--image"],
-                        digits_path=args["--digits"],
-                        width=int(args["--width"]),
-                        height=int(args["--height"]),
-                        bg=args["--bg"].upper(),
-                        fg=args["--fg"].upper(),
-                        number_bg=args["--nbg"].upper(),
-                        number_fg=args["--nfg"].upper())
+    aax = AAx(image_path=args["--image"],
+              digits_path=args["--digits"],
+              bg=args["--bg"].upper(),
+              fg=args["--fg"].upper(),
+              x_bg=args["--xbg"].upper(),
+              x_fg=args["--xfg"].upper())
 
-    print(out)
+    frame = aax.run(width=int(args["--width"]), height=int(args["--height"]))
+    print(frame)
 
-    if not args["--no-summary"]:
+    if args["--summary"]:
+        table = aax.summary_table()
         print("")
-        print("")
-        print("How many digits use for X and how many for the BG:")
-        print("For the Number X:")
-        for x_digit, freq in fgd.items():
-            if freq > 0:
-                text = str2bg(args["--nbg"].upper()) + str2fg(args["--nfg"].upper()) + str(x_digit)
-                print("XFG:", text, "x", freq)
-        print("For the Background:")
-        for x_digit, freq in bgd.items():
-            if freq > 0:
-                text = str2bg(args["--bg"].upper()) + str2fg(args["--fg"].upper()) + str(x_digit)
-                print("BG:", text, "x", freq)
-
+        print("Summary Table w/frequencies")
+        print(table)
